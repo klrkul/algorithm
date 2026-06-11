8872aff7902b6874ec0c8dd586cbf11e34ae02f4
klrkul <liubovruslanovna@gmail.com>
2026-06-11 23:47:03 +1000

    problem solved

--- lab_3/task704.py
+++ lab_3/task704.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def search(self, nums, target):
-        left=0
-        right =len(nums) - 1
+        left = 0
+        right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
