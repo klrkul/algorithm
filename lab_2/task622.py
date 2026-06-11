c781202289c09634caf1fbec90a2b7eede1d76b9
klrkul <liubovruslanovna@gmail.com>
2026-06-11 23:59:07 +1000

    problem solved

---
+++ lab_2/task622.py
@@ -0,0 +1,35 @@
+class MyCircularQueue(object):
+
+    def __init__(self, k):
+        self.queue = []
+        self.max_size = k
+
+    def enQueue(self, value):
+        if len(self.queue)<self.max_size:
+            self.queue.append(value)
+            return True
+        else:
+            return False
+
+    def deQueue(self):
+        if len(self.queue)>0:
+            self.queue.pop(0)
+            return True
+        else:
+            return False
+
+    def Front(self):
+        if len(self.queue) == 0:
+            return -1
+        return self.queue[0]
+
+    def Rear(self):
+        if len(self.queue)==0:
+            return -1
+        return self.queue[-1]
+
+    def isEmpty(self):
+        return len(self.queue)==0
+
+    def isFull(self):
+        return len(self.queue)==self.max_size
