2cb51002a19787d2dca8fa6846eaaff6ed1db7ca
klrkul <liubovruslanovna@gmail.com>
2026-06-12 14:01:22 +1000

    problem solved

--- lab_6/task417.py
+++ lab_6/task417.py
@@ -1,14 +1,35 @@
 class Solution(object):
-    def isValidBST(self, root):
-        def validate(node, low=None, high=None):
-            if not node:
-                return True
-            if low is not None and node.val <= low:
-                return False
-            if high is not None and node.val >= high:
-                return False
-            if not validate(node.left, low, node.val):
-                return False
-            return validate(node.right, node.val, high)
+    def pacificAtlantic(self, heights):
+        if not heights or not heights[0]:
+            return []
 
-        return validate(root)
+        m, n = len(heights), len(heights[0])
+        pacific = [[False] * n for _ in range(m)]
+        atlantic = [[False] * n for _ in range(m)]
+
+        def dfs(r, c, visited):
+            visited[r][c] = True
+            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if (0 <= nr < m and 0 <= nc < n and
+                        not visited[nr][nc] and
+                        heights[nr][nc] >= heights[r][c]):
+                    dfs(nr, nc, visited)
+
+        for i in range(m):
+            dfs(i, 0, pacific)
+        for j in range(n):
+            dfs(0, j, pacific)
+        for i in range(m):
+            dfs(i, n - 1, atlantic)
+        for j in range(n):
+            dfs(m - 1, j, atlantic)
+        result = []
+        for i in range(m):
+            for j in range(n):
+                if pacific[i][j] and atlantic[i][j]:
+                    result.append([i, j])
+
+        return result
