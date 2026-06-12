ab356c58e832a368a88c5f9a503676c4adf77183
klrkul <liubovruslanovna@gmail.com>
2026-06-12 13:48:39 +1000

    problem solved

---
+++ lab_7/task300.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        if not nums:
+            return 0
+        tails = []
+
+        for num in nums:
+            left, right = 0, len(tails)
+            while left < right:
+                mid = (left + right) // 2
+                if tails[mid] < num:
+                    left = mid + 1
+                else:
+                    right = mid
+
+            if left == len(tails):
+                tails.append(num)
+            else:
+                tails[left] = num
+
+        return len(tails)
