7d94109ef1d80cfcb7e2d085a37a3e1f3743918b
klrkul <liubovruslanovna@gmail.com>
2026-06-12 13:57:29 +1000

    problem solved

---
+++ lab_5/task98.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValidBST(self, root):
+        def validate(node, low=None, high=None):
+            if not node:
+                return True
+            if low is not None and node.val <= low:
+                return False
+            if high is not None and node.val >= high:
+                return False
+            if not validate(node.left, low, node.val):
+                return False
+            return validate(node.right, node.val, high)
+
+        return validate(root)
