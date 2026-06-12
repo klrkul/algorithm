1bdd73cc2284642e79972f214a1986224c51cc46
klrkul <liubovruslanovna@gmail.com>
2026-06-12 13:42:57 +1000

    problem solved

---
+++ lab_5/task102.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def levelOrder(self, root):
+        res = []
+
+        def dfs(node, level):
+            if not node:
+                return
+            if len(res) == level:
+                res.append([])
+            res[level].append(node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        return res
