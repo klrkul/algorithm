685569c8d606e0dfdb693eead01a2bbe245df3aa
klrkul <liubovruslanovna@gmail.com>
2026-06-12 14:07:08 +1000

    problem solved

---
+++ lab_8/task79.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def exist(self, board, word):
+        m, n = len(board), len(board[0])
+
+        def dfs(r, c, index):
+            if index == len(word):
+                return True
+            if (r < 0 or r >= m or c < 0 or c >= n or
+                    board[r][c] != word[index]):
+                return False
+            temp = board[r][c]
+            board[r][c] = '#'
+            found = (dfs(r + 1, c, index + 1) or
+                     dfs(r - 1, c, index + 1) or
+                     dfs(r, c + 1, index + 1) or
+                     dfs(r, c - 1, index + 1))
+            board[r][c] = temp
+
+            return found
+
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0):
+                    return True
+
+        return False
